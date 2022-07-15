6a2d21ce35383404164f5bcce7c862796d7bf58c
Kimani <elvis.kimani@student.moringaschool.com>
2022-07-15 15:27:05 +0300

    commit tree 166330dc556c773123c7e0b91d68ca154abea2a4

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setinCart] = useState(false);
+
+  const itemClass = inCart? "in-cart" : "";
+
+  const addRemove = inCart? "add" : "remove";
+
+  function handleCart(){
+    setinCart((inCart) => !inCart)
+  }
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addRemove} onClick={handleCart}>{inCart? "Add to Cart": "Remove From Cart"}</button>
     </li>
   );
 }
